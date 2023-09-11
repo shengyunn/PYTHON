a9d6e13cd6be458c947a62e697823735db34a077
shengyunn <ryan082795@gmail.com>
2023-09-11 11:17:38 +0800

    Yun

--- ctrl.py
+++ ctrl.py
@@ -2,11 +2,5 @@ import pyautogui as ctrl
 import sys
 import time
 
-
-for i in range(60):
-    print(ctrl.position())
-    time.sleep(0.5)
-    if ctrl.mouseDown(button='right'):
-        print("右鍵被點擊，程式結束。")
-        break   
-
+ctrl.moveTo(1469,516)
+ctrl.click(clicks=1000)
